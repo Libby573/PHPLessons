b1ddb66654bd5c95a8992a13dc8a62f7ec248a71
Libby Simpson <Lee.Simpson@students.jmcss.org>
2024-03-22 08:16:28 -0500

    Video7

---
+++ datatypes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //String
+    $name = 'Today is Friday';
+
+    //Integer
+    $name = 15;
+
+    //Float 
+    $name = 15.859;
+
+    //Boolean
+    true = 1
+    false = 0
+
+    //Array
+    $names = array("Libby", "Anna", "Jessi");
+    echo $names['2'];
+    ?>
+</body>
+</html>
