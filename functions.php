f2ec4791eb0ce236399b8250230669f6db2982ab
Libby Simpson <113134952+Libby573@users.noreply.github.com>
2024-04-03 07:34:08 -0500

    FUNCTION

--- functions.php
+++ functions.php
@@ -12,5 +12,21 @@
     <?php
         echo str_replace("Libby", "Jessi", "Hi Libby")
     ?>
+    <?php
+
+    $x = 100;
+
+        function newCalc($x) {
+            $newnr = $x * 0.75;
+            echo "Here is 75% of what you wrote: " .$newnr;
+        }
+        $x = 100;
+        newCalc($x);
+        
+        echo "<br>";
+
+        $a = 10;
+        newCalc($a);
+    ?>
 </body>
 </html>
