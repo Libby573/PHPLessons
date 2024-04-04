7d9da7a3cd513c39a96cf6fb5ae53b2925f6e190
Libby Simpson <113134952+Libby573@users.noreply.github.com>
2024-04-04 08:35:43 -0500

    pushhhh

---
+++ includes/header.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="calculator.php">Calculator</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="calendar.php">Calendar</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="conditionals.php">Conditionals</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="datatypes.php">Datatypes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="functions.php">Functions</a>
+          <li class="nav-item">
+          <a class="nav-link" href="index.php">Index</a>
+          <li class="nav-item">
+          <a class="nav-link" href="loops.php">Loops</a>
+          <li class="nav-item">
+          <a class="nav-link" href="operators.php">Operators</a>
+        </li>
+        </li>
+        </li>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
