b5501f254edeeea6420a0ed54d624b61a2fe51b0
Libby Simpson <113134952+Libby573@users.noreply.github.com>
2024-04-10 07:56:07 -0500

    pushH

--- includes/header.php
+++ includes/header.php
@@ -1,25 +1,41 @@
-<?php
-  session_start()
-?>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">Index</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#"></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="calculator.php">Calculator</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="calendar.php">Calendar</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        <a class="nav-link" href="conditionals.php">Conditionals</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="datatypes.php">Datatypes</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="functions.php">Functions</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="index.php">Index</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="loops.php">Loops</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="operators.php">Operators</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="superglobals.php">Superglobals</a>
         </li>
       </ul>
     </div>
