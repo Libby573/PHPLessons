37c502dfd880c5f4d163dfa6f4742ea23c1693cc
Libby Simpson <113134952+Libby573@users.noreply.github.com>
2024-04-22 08:43:46 -0500

    pushhh

--- includes/header.php
+++ includes/header.php
@@ -64,6 +64,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel ="stylesheet" href ="style.css">
 </head>
 <body>
   <header>
@@ -71,14 +72,23 @@
 <a href ='#'>
   <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="Portfolio"></a></li>
-    <li><a href="About me"></a></li>
-    <li><a href="Contact"></a></li>
+    <li><a href="#">Portfolio</a></li>
+    <li><a href="#">About me</a></li>
+    <li><a href="#">Contact</a></li>
 </ul>
 <div>
-  <form action ="" method ="post">
-    
+  <form action ="includes/login.inc.php" method ="post">
+    <input type="text" name="mailuid" placeholder="Username/E-mail...">
+    <input type="password" name="pwd" placeholder="Password...">
+    <button type ="submit" name= "login-submit">Login</button>
+
 </form>
+
+<a href="signup.php">Signup</a>
+<form action ="includes/logout.inc.php" method="post">
+<button type ="submit" name = "logout-submit"> Logout</button>
+</form>
+
 </div>
 </nav>
 
--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -10,4 +10,23 @@ VALUES ('$first', '$last', '$email', '$uid', '$pwd');";
 mysqli_query($conn, $sql);
 
 header ("Location: ../index.php?signup=success");
+?> 
+<?php
+require "header.php";
+?>
+<main>
+    <div class ="wrapper-main">
+        <section class="section-default">
+            <h1> Signup</h1>
+            <form action ="includes/signup.inc.php" method="post">
+            <input type = "text" name="uid" placeholder ="Username">
+            <input type = "text" name="mail" placeholder ="E-mail">
+            <input type = "password" name ="pwd" placeholder ="Password">
+            <input type = "password" name ="pwd-repeat" placeholder =" Repeat password">
+            <button type ="sumbit" name="signup-submit">Signup</button>
+</section>
+</div>
+</main>
+<?php
+require "footer.php";
 ?>
