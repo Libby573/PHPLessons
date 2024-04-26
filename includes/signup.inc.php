fc6b1b9f4802b5a3a2fe99108bb37a22b5611736
Libby Simpson <113134952+Libby573@users.noreply.github.com>
2024-04-26 08:41:30 -0500

    push

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -15,10 +15,36 @@ header ("Location: ../index.php?signup=success");
 require "header.php";
 ?>
 <main>
+    
     <div class ="wrapper-main">
         <section class="section-default">
             <h1> Signup</h1>
-            <form action ="includes/signup.inc.php" method="post">
+            <?php
+            if (isset($_GET['error'])){
+                if ($_GET['error'] == "emptyfields"){
+                    echo '<p class = "signuperror"> Fill in all fields!</p>';
+                }
+                if ($_GET['error'] == "invaliduidmail"){
+                    echo '<p class = "signuperror"> Fill in all fields!</p>';
+                }
+                if ($_GET['error'] == "invaliduid"){
+                    echo '<p class = "signuperror"> Fill in all fields!</p>';
+                }
+                if ($_GET['error'] == "invalidmail"){
+                    echo '<p class = "signuperror"> Fill in all fields!</p>';
+                }
+                if ($_GET['error'] == "passwordcheck"){
+                    echo '<p class = "signuperror"> Fill in all fields!</p>';
+                }
+                if ($_GET['error'] == "usertaken"){
+                    echo '<p class = "signuperror"> Fill in all fields!</p>';
+                }
+            }
+            else if ($_GET["signup"] == "success") {
+                echo'<p class="signupsuccess">Signup successful!</p>';
+            }
+            ?>
+            <form class ="includes/signup.inc.php" method="post">
             <input type = "text" name="uid" placeholder ="Username">
             <input type = "text" name="mail" placeholder ="E-mail">
             <input type = "password" name ="pwd" placeholder ="Password">
